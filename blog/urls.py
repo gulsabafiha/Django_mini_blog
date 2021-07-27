28029af55bbe63844d75a61509c960bb80583df6
gulsaba fiha <gulsabafiha001@gmail.com>
2021-07-27 13:41:46 +0600

    90% done

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
    path('contact/',views.contact,name='contact'),
    path('dashboard/',views.dashboard,name='dashboard'),
    path('logout/',views.user_logout,name='logout'),
-    path('signup/',views.signup,name='signup'),
-   path('login/',views.login,name='login'),
+   path('signup/',views.signup,name='signup'),
+   path('login/',views.user_login,name='login'),
+   path('addpost/',views.add_post,name='addpost'),
+   path('update/<int:id>/',views.update_post,name='updatepost'),
+   path('delete/<int:id>/',views.delete_post,name='deletepost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django import forms
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.messages.api import success
 from django.shortcuts import render,HttpResponseRedirect
-from . forms import SignUpForm
+from . forms import LoginForm, PostForm, SignUpForm
+from django.contrib import messages
+from . models import Post
 
 
 # home views
 def home(request):
-    return render (request,'blog/home.html')
+    posts= Post.objects.all()
+    return render (request,'blog/home.html',{'posts':posts})
 
 
 def about(request):
@@ -15,16 +20,92 @@ def about(request):
 def contact(request):
     return render (request,'blog/contact.html')
 
+
+#Dashboard View
 def dashboard(request):
-    return render (request,'blog/dashboard.html')
+    if request.user.is_authenticated:
+        posts=Post.objects.all()
+        return render (request,'blog/dashboard.html',{'posts':posts})
+    else:
+        return HttpResponseRedirect('/login/')
 
+
+#Logout View
 def user_logout(request):
-   return HttpResponseRedirect('/')
+    logout(request)
+    return HttpResponseRedirect('/')
 
+
+#Sign Up View
 def signup(request):
-    form= SignUpForm()
+    if request.method == 'POST':
+        form= SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,"Congratulations!! You have Become an Author")
+            form.save()
+    else:
+        form= SignUpForm()
     return render (request,'blog/signup.html',{'form':form})
 
+#Login View
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form= LoginForm(request=request,data=request.POST)
+            if form.is_valid():
+                uname=form.cleaned_data['username']
+                upass=form.cleaned_data['password']
+                user =authenticate(username=uname,password=upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,"Logged in Successfully!!")
+                    return HttpResponseRedirect('/dashboard/')
+        else:
+            form= LoginForm()
+        return render (request,'blog/login.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
 
-def login(request):
-    return render (request,'blog/login.html')
+
+#Add View 
+def add_post(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form=PostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect('/dashboard/')
+        else:
+            form= PostForm()
+        return render (request,'blog/addpost.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+#Update/Edit View
+def update_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi= Post.objects.get(pk=id)
+            form = PostForm(request.POST, instance=pi)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect('/dashboard/')
+        else:
+            pi= Post.objects.get(pk=id)
+            form=PostForm(instance=pi)
+        return render (request,'blog/update.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+
+#Delete View
+def delete_post(request,id ):
+    if request.user.is_authenticated:
+        if request.method =='POST':
+            pi= Post.objects.get(pk=id)
+            pi.delete()
+            return HttpResponseRedirect('/dashboard/')
+    else:
+        return HttpResponseRedirect('/login/')
